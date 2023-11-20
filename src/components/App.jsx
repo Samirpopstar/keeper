2758ef6309f4a9a107b1719fb0e14df8aa07c4c5
Samirpopstar <coolsamiracharya.jt@gmail.com>
2023-11-20 18:54:03 +0545

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  const [item, setItem] = useState("")
+
+  useEffect(()=>{
+    fetch("https://official-joke-api.appspot.com/jokes/random")
+    .then(res => res.json())
+    .then(json => setItem(json))
+  },[])
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div>
+      <Header />
+      <h1>{item.setup}</h1>
+      <h1>{item.punchline}</h1>
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
