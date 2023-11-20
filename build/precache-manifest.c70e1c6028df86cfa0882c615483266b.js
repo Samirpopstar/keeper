e19dcc3cec892224d1c9df127af437de2bf7f146
Samirpopstar <coolsamiracharya.jt@gmail.com>
2023-11-20 19:14:57 +0545

    react-app-added

---
+++ build/precache-manifest.c70e1c6028df86cfa0882c615483266b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6421e55ea4c50a03b040e0d9179532d9",
+    "url": "/Samirpopstar/keeper-app/index.html"
+  },
+  {
+    "revision": "38df1e954b9ae453b25e",
+    "url": "/Samirpopstar/keeper-app/static/js/2.d8908c0d.chunk.js"
+  },
+  {
+    "revision": "1f77ef9511da9905a388",
+    "url": "/Samirpopstar/keeper-app/static/js/main.0682f8b9.chunk.js"
+  },
+  {
+    "revision": "f04c0221d7eac1057713",
+    "url": "/Samirpopstar/keeper-app/static/js/runtime-main.c5e3cd60.js"
+  }
+]);
