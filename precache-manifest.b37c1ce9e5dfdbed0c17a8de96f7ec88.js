69b10f645347947fa7ff0f72202873c9178f5664
Samirpopstar <coolsamiracharya.jt@gmail.com>
2023-11-20 19:18:44 +0545

    Updates

---
+++ precache-manifest.b37c1ce9e5dfdbed0c17a8de96f7ec88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da192eea95236fdaca9d7761940370b",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "38df1e954b9ae453b25e",
+    "url": "/keeper/static/js/2.d8908c0d.chunk.js"
+  },
+  {
+    "revision": "1f77ef9511da9905a388",
+    "url": "/keeper/static/js/main.0682f8b9.chunk.js"
+  },
+  {
+    "revision": "ee38aac5cc265be45431",
+    "url": "/keeper/static/js/runtime-main.d35bd170.js"
+  }
+]);
